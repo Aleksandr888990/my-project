aa77c9eb2622b85ae271835c2e38b5ad16207f04
Alex <fannut1@gmail.com>
2023-01-24 19:55:47 +0300

    create react with jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = React.createElement('h1', {id:'title'}, 'Hello React')
-console.log(h1)
+// const h1 = React.createElement('h1', {id:'title'}, 'Hello React')
+// console.log(h1)
 
-const list = React.createElement(
-    'ul',
-    null,
-    React.createElement('li', null, 'list Item 1'),
-    React.createElement('li', null, 'list Item 2'),
-    React.createElement('li', null, 'list Item 3')
+// const list = React.createElement(
+//     'ul',
+//     null,
+//     React.createElement('li', null, 'list Item 1'),
+//     React.createElement('li', null, 'list Item 2'),
+//     React.createElement('li', null, 'list Item 3')
+// )
+const h1 = <h1 id="title">Hello React.js</h1>
+const list = (
+    <ul>
+        <li>list Item 1</li>
+        <li>list Item 2</li>
+        <li>list Item 3</li>
+    </ul>
 )
 
 const content = (
