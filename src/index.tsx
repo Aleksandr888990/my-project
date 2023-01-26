c11753171178562febc0fa5525a4195f1d6e973d
Alex <fannut1@gmail.com>
2023-01-26 20:41:08 +0300

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Content} from './Content'
-import Title from './Title'
 
 
 
-
-function App () {
-  return (
-  <>
-      <Title text="React" />
-      <Title text="TS" />
-      <Content text1="Hello world" text2="Hello world2" year={2023}/>
-  </>
-  )
-}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
